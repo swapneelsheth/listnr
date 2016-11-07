2678c6f49ae25154cbb58aee530e5d8f03aa3a05
Swapneel Sheth <swapneel@cis.upenn.edu>
2016-11-07 16:59:52 -0500

    running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102200004) do
+ActiveRecord::Schema.define(version: 20161102200321) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20161102200004) do
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "genre"
   end
 
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"

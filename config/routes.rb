5d2a89a27d5845b6cfb929d764332e226f44aafe
Swapneel Sheth <swapneel@cis.upenn.edu>
2016-11-09 19:57:12 -0500

    annotating routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
+# == Route Map
+#
+#        Prefix Verb   URI Pattern                    Controller#Action
+#          root GET    /                              artists#index
+#       reviews GET    /reviews(.:format)             reviews#index
+#        review GET    /reviews/:id(.:format)         reviews#show
+#         songs GET    /songs(.:format)               songs#index
+#               POST   /songs(.:format)               songs#create
+#      new_song GET    /songs/new(.:format)           songs#new
+#     edit_song GET    /songs/:id/edit(.:format)      songs#edit
+#          song GET    /songs/:id(.:format)           songs#show
+#               PATCH  /songs/:id(.:format)           songs#update
+#               PUT    /songs/:id(.:format)           songs#update
+#               DELETE /songs/:id(.:format)           songs#destroy
+# rating_artist GET    /artists/:id/rating(.:format)  artists#rating
+#               GET    /artists/sort/:field(.:format) artists#sort
+#       artists GET    /artists(.:format)             artists#index
+#               POST   /artists(.:format)             artists#create
+#    new_artist GET    /artists/new(.:format)         artists#new
+#   edit_artist GET    /artists/:id/edit(.:format)    artists#edit
+#        artist GET    /artists/:id(.:format)         artists#show
+#               PATCH  /artists/:id(.:format)         artists#update
+#               PUT    /artists/:id(.:format)         artists#update
+#               DELETE /artists/:id(.:format)         artists#destroy
+#
+
 Rails.application.routes.draw do
   # resources :reviews, except: :destroy
   
